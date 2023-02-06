7a374dbb949f8e7c0375b866d0de57ac0f2381ba
Ayush Rawat <90140818+ayushrwt1@users.noreply.github.com>
2023-02-06 23:55:09 +0530

    removing api key

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '9a9751dcb1e165ea462e4af782a3b5df',
+    apiKey: 'API KEY',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
